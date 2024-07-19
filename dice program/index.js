6f4e162d6bfd0803b599f6b5ccb3d9059acf69dd
João Pereira <jrbspcode@gmail.com>
2024-07-19 10:14:13 +0200

    Add files via upload

--- dice program/index.js
+++ dice program/index.js
@@ -1,17 +1,23 @@
 function rollDice (){
     const numOfDice = document.getElementById("numOfDice").value;
     const diceResult = document.getElementById("diceResult");
+    const diceResult2 = document.getElementById("diceResult2");
     const diceImages = document.getElementById("diceImages");
     const values = [];
     const images = [];
+    let total = 0;
+    Number(total);
 
     for(let i = 0; i < numOfDice; i++){
         const value = Math.floor(Math.random() * 6)+1;
-        console.log(value);
         values.push(value);
+        total += value;
+        console.log(total);
         images.push(`<img src="images/${value}.png">`);
     }
+    
 
-    diceResult.textContent = `dice: ${values.join(', ')}`;
+    diceResult.textContent = `Dice: ${values.join(', ')}`;
+    diceResult2.textContent = "Total: "+ total;
     diceImages.innerHTML = images.join('');
 }
