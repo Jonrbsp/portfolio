b8ddc65b05a08de568f2dc425fb59ed985186c68
João Pereira <jrbspcode@gmail.com>
2024-07-23 10:59:07 +0200

    Add files via upload

---
+++ photography.js
@@ -0,0 +1,56 @@
+function hoverwed(wedding) {
+    wedding.setAttribute('src', './photography pages/wedding img/main-hover.jpg');
+  }
+  
+  function unhoverwed(wedding) {
+    wedding.setAttribute('src', './photography pages/wedding img/main.jpg');
+  }
+
+
+  function hoverpor(portrait) {
+    portrait.setAttribute('src', './photography pages/portrait img/main-hover.jpg');
+  }
+  
+  function unhoverpor(portrait) {
+    portrait.setAttribute('src', './photography pages/portrait img/main.jpg');
+  }
+
+  function hovercou(couple) {
+    couple.setAttribute('src', './photography pages/couple img/main-hover.jpg');
+  }
+  
+  function unhovercou(couple) {
+    couple.setAttribute('src', './photography pages/couple img/main.jpg');
+  }
+
+  function hoverstu(studio) {
+    studio.setAttribute('src', './photography pages/studio img/main-hover.jpg');
+  }
+  
+  function unhoverstu(studio) {
+    studio.setAttribute('src', './photography pages/studio img/main.jpg');
+  }
+
+  function hoverpre(pregnancy) {
+    pregnancy.setAttribute('src', './photography pages/pregnancy img/main-hover.jpg');
+  }
+  
+  function unhoverpre(pregnancy) {
+    pregnancy.setAttribute('src', './photography pages/pregnancy img/main.jpg');
+  }
+
+  function hoverchi(children) {
+    children.setAttribute('src', './photography pages/children img/main-hover.jpg');
+  }
+  
+  function unhoverchi(children) {
+    children.setAttribute('src', './photography pages/children img/main.jpg');
+  }
+
+  function hovermak(makeup) {
+    makeup.setAttribute('src', './photography pages/makeup img/main-hover.jpg');
+  }
+  
+  function unhovermak(makeup) {
+    makeup.setAttribute('src', './photography pages/makeup img/main.jpg');
+  }
